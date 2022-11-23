3f85043fa9a1e31acc57f77c953c6a562c83de2f
Parth Shah <parth@polimorphic.com>
2022-11-23 02:34:34 -0800

    add max width and max height

--- src/Text/HTML/SanitizeXSS/Css.hs
+++ src/Text/HTML/SanitizeXSS/Css.hs
@@ -114,9 +114,9 @@ allowed_css_properties = fromList acceptable_css_properties
       "border-left-color", "border-right-color", "border-top-color", "clear",
       "color", "cursor", "direction", "display", "elevation", "float", "font",
       "font-family", "font-size", "font-style", "font-variant", "font-weight",
-      "height", "letter-spacing", "line-height", "overflow", "pause",
-      "pause-after", "pause-before", "pitch", "pitch-range", "richness",
-      "speak", "speak-header", "speak-numeral", "speak-punctuation",
+      "height", "letter-spacing", "line-height", "max-height", "max-width",
+      "overflow", "pause", "pause-after", "pause-before", "pitch", "pitch-range",
+      "richness", "speak", "speak-header", "speak-numeral", "speak-punctuation",
       "speech-rate", "stress", "text-align", "text-decoration", "text-indent",
       "unicode-bidi", "vertical-align", "voice-family", "volume",
       "white-space", "width"]
